29239dc631c3c520a164a1e45659d1a086b77f65
DESKTOP-3GBPLU2\병욱 <dlquddnr414@naver.com>
2021-01-11 17:23:35 +0900

    2021-01-11

---
+++ 013_Data6(실수형데이터)/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _013_Data6_실수형데이터_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float a = 3.143127301237123123123123123f;
+            Console.WriteLine("a: {0}", a);
+
+            double b = 3.143127301237123123123123123;
+            Console.WriteLine("b: {0}", b);
+
+            decimal c = 3.143127301237123123123123123m;
+            Console.WriteLine("c: {0}", c);
+        }
+    }
+}
--- 033_Check/Program.cs
+++ 033_Check/Program.cs
@@ -11,7 +11,7 @@ namespace _033_Check
         static void Main(string[] args)
         {
             Console.Write("국어 점수를 입력하세요?");
-            string strLan = Console.ReadLine();
+            string strKor = Console.ReadLine();
             Console.Write("영어 점수를 입력하세요?");
             string strEng = Console.ReadLine();
             Console.Write("수학 점수를 입력하세요?");
@@ -19,15 +19,15 @@ namespace _033_Check
             Console.Write("과학 점수를 입력하세요?");
             string strSci = Console.ReadLine();
 
-            int Lan = int.Parse(strLan);
+            int Kor = int.Parse(strKor);
             int Eng = int.Parse(strEng);
             int Math = int.Parse(strMath);
             int Sci = Convert.ToInt32(strSci);
 
-            int sum = Lan + Eng + Math + Sci;
+            int sum = Kor + Eng + Math + Sci;
             float avg = (float)sum / 4;
 
-            Console.WriteLine("국어: {0}   영어: {1}   수학: {2}   과학: {3}", Lan, Eng, Math, Sci);
+            Console.WriteLine("국어: {0}   영어: {1}   수학: {2}   과학: {3}", Kor, Eng, Math, Sci);
             Console.WriteLine("총점: {0}   평균: {1}", sum, avg);
         }
     }
