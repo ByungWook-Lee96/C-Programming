65c0bf8b15a802a5256a71b3760f26726d15feb6
DESKTOP-3GBPLU2\병욱 <dlquddnr414@naver.com>
2021-01-16 02:26:30 +0900

    20210115

---
+++ 075_Check/Program.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _075_Check
+{
+    class Program
+    {
+        static void InputID(int[] ID, int index)
+        {
+            Console.Write("학생 ID를 입력하세요?");
+            ID[index] = int.Parse(Console.ReadLine());
+        }
+        static void InputKor(int[] kor, int index)
+        {
+            Console.Write("국어 점수를 입력하세요?");
+            kor[index] = int.Parse(Console.ReadLine());
+        }
+        static void InputMath(int[] math, int index)
+        {
+            Console.Write("수학 점수를 입력하세요?");
+            math[index] = int.Parse(Console.ReadLine());
+        }
+        static void InputEng(int[] eng, int index)
+        {
+            Console.Write("영어 점수를 입력하세요?");
+            eng[index] = int.Parse(Console.ReadLine());
+
+            Console.WriteLine();
+
+        }
+        static void PrintID(int[] ID)
+        {
+            Console.WriteLine("학생 ID: " + ID[0]);
+            Console.WriteLine("학생 ID: " + ID[1]);
+            Console.WriteLine("학생 ID: " + ID[2]);
+            
+        }
+        static int CheckID(int id,int[] ID)
+        {         
+            for (int i = 0; i < ID.Length; i++)
+            {
+                if (id == ID[i])
+                {
+                    return i;
+                }
+            }
+            Console.WriteLine("학생 아이디가 없어요. 다시 입력하세요.");
+            return -1;
+        }
+        static void Main(string[] args)
+        {
+            int[] ID = new int[3];
+            int[] kor = new int[3];
+            int[] math = new int[3];
+            int[] eng = new int[3];
+            int id = 0;
+
+            for (int i = 0; i < 3; i++)
+            {
+                InputID(ID, i);
+                InputKor(kor, i);
+                InputMath(math, i);
+                InputEng(eng, i);
+            }
+            Console.Clear();
+
+            while (true)
+            {
+                PrintID(ID);
+                Console.Write("학생 아이디를 입력하세요? <0>나가기");
+                id = int.Parse(Console.ReadLine());
+                if (id == 0)
+                {
+                    break;
+                }
+                if(CheckID(id, ID) == -1)
+                {
+                    continue;
+                }
+                Console.WriteLine("국어 점수: " + kor[CheckID(id, ID)]);
+                Console.WriteLine("수학 점수: " + math[CheckID(id, ID)]);
+                Console.WriteLine("영어 점수: " + eng[CheckID(id, ID)]);
+                int sum = kor[CheckID(id, ID)] + math[CheckID(id, ID)] + eng[CheckID(id, ID)];
+                float avg = sum / 3f;
+                Console.WriteLine("총점: " + sum);
+                Console.WriteLine("평균: " + avg);
+
+                
+            }
+
+
+        }
+    }
+}
---
+++ 076_Check/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _076_Check
+{
+    class Program
+    {
+        
+        static int InputNumber(int count)
+        {
+           
+            if (count == 0)
+            {
+                Console.Write("첫번째 수를 입력 해 주세요? "); 
+            }
+            else
+            {
+                Console.Write("두번째 수를 입력 해 주세요? ");
+            }
+            
+            return int.Parse(Console.ReadLine());
+        }
+        static void PrintResult(int a, int b)
+        {
+            Console.WriteLine(a + " + " + b + " = " + (a + b));           
+        }
+        static bool CheckEnd()
+        {
+            Console.Write("추가로 계산할까요(1: OK, 0: NO, 단 총 10번까지 가능)");
+            if (int.Parse(Console.ReadLine()) == 0)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+        static void Main(string[] args)
+        {
+            int count = 0;
+            int[] firstNum = new int[10];
+            int[] secondNum = new int[10];
+            
+            while (true)
+            {
+                firstNum[count]=InputNumber(0);
+                secondNum[count] = InputNumber(1);
+
+                PrintResult(firstNum[count], secondNum[count]);
+                count++;
+
+                if(count>=10 || CheckEnd() == false)
+                {
+                    for (int i = 0; i < count; i++)
+                    {
+                        PrintResult(firstNum[i], secondNum[i]);
+                    }
+                    break;
+                }
+
+
+            }
+        }
+    }
+}
