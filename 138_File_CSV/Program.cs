6d2af30a75983db1dc35edbc14fcb13006af2d9a
DESKTOP-3GBPLU2\병욱 <dlquddnr414@naver.com>
2021-01-25 16:26:07 +0900

    20210125

---
+++ 138_File_CSV/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace _138_File_CSV
+{
+    class Stage
+    {
+        public string stage { get; set; }
+        public int min { get; set; }
+        public int max { get; set; }
+        public int finish { get; set; }
+        public int gold { get; set; }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string fileName = "test.csv";
+
+            int index = 0;
+            List<Stage> listStage = new List<Stage>();
+
+            using(StreamReader sr = new StreamReader(new FileStream(fileName, FileMode.Open)))
+            {
+                while (false == sr.EndOfStream)
+                {
+                    string readStr = sr.ReadLine();
+
+                    if(index++ >= 1)  //index가 0일때는 칼럼명이 들어있기 때문에 1부터 받기 시작
+                    {
+                        string[] splitData = readStr.Split(',');
+
+                        Stage temp = new Stage();
+                        temp.stage = splitData[0];
+                        temp.min = Convert.ToInt32(splitData[1]);
+                        temp.max = Convert.ToInt32(splitData[2]);
+                        temp.finish = Convert.ToInt32(splitData[3]);
+                        temp.gold = Convert.ToInt32(splitData[4]);
+                        listStage.Add(temp);
+                    }
+                }
+            }
+            foreach(var d in listStage)
+            {
+                Console.Write("{0} {1} {2} {3} {4}", d.stage, d.min, d.max, d.finish, d.gold);
+                Console.WriteLine();
+            }
+
+            string str = "0,1,2,3,4,5";
+            string[] splitRead = str.Split(',');
+
+            for (int i = 0; i < splitRead.Length; i++)
+            {
+                Console.WriteLine("  {0}  ", splitRead[i]);
+            }
+        }
+    }
+}
---
+++ Hangman/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace Hangman
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Hang();
+            Thread.Sleep(300);
+            Console.Clear();
+            Head();
+            
+        }
+        static void Hang()
+        {
+            Console.WriteLine("");
+            Console.WriteLine("");
+            Console.WriteLine("");
+            Console.WriteLine("\t\t\t\t _______________");
+            for (int i = 0; i < 10; i++)
+            {
+                if (i > 1)
+                {
+                    Console.WriteLine("\t\t\t\t| ");
+                }
+                else
+                {
+                    Console.WriteLine("\t\t\t\t|\t\t|");  //Console.WriteLine("\t\t\t\t|\t\t|");  위에서 7번째 옆에서는 \t\t\t\t|\t\t하고 얼굴 만들기
+
+                }
+            }
+            Console.WriteLine("\t\t\t   -----------");
+
+        }
+        static void Head()
+        {
+            Console.WriteLine("");
+            Console.WriteLine("");
+            Console.WriteLine("");
+            Console.WriteLine("\t\t\t\t _______________");
+            for (int i = 0; i < 10; i++)
+            {
+                if (i > 2)
+                {
+                    Console.WriteLine("\t\t\t\t| ");
+                }
+                else if (i == 2)
+                {
+                    Console.WriteLine("\t\t\t\t|\t        ○");
+
+                }
+                else
+                {
+                    Console.WriteLine("\t\t\t\t|\t\t|");  //Console.WriteLine("\t\t\t\t|\t\t|");  위에서 7번째 옆에서는 \t\t\t\t|\t\t하고 얼굴 만들기
+
+                }
+            }
+            Console.WriteLine("\t\t\t   -----------");
+        }
+    }
+}
